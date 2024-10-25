c0229372b84a5f4a5e8e38ab4b3c675b8630d322
Riad Mahi <riad.mahi@remedee.com>
2024-10-25 13:28:10 +0200

    Setup project

---
+++ src/app/home/presentation-section/presentation-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './presentation-section.component.html',
+  styleUrl: './presentation-section.component.scss'
+})
+export class PresentationSectionComponent {
+
+}
