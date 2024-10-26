0a179c34ef3f497c3c754827954e377c1d45eff3
Riad Mahi <riad.mahi@remedee.com>
2024-10-26 14:03:47 +0200

    Add navigatio back

--- src/app/portfolio/hobdating/hobdating.component.ts
+++ src/app/portfolio/hobdating/hobdating.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hobdating',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './hobdating.component.html',
   styleUrl: './hobdating.component.scss'
 })
